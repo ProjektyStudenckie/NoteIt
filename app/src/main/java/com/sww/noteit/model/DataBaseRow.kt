db517e63a7b622fead1a9a07b3da7a3809095fed
wielok <wielochowskiwojciech@gmail.com>
2021-05-16 21:28:06 +0200

    add firebase to project

---
+++ app/src/main/java/com/sww/noteit/model/DataBaseRow.kt
@@ -0,0 +1,9 @@
+package com.sww.noteit.model
+
+data class DataBaseRow (val ID: Int = 0,
+                        var UserID: String =" ",
+                        var Title: String =" ",
+                        var Note: String = " ",
+                        var DateDate: String =" ",
+                        var ImageURL: String = " ")
+
--- app/src/main/java/com/sww/noteit/model/DataContainer.kt
+++ app/src/main/java/com/sww/noteit/model/DataContainer.kt
@@ -3,6 +3,8 @@ package com.sww.noteit.model
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import okhttp3.ResponseBody
 import java.time.format.DateTimeFormatter
 import java.util.*
@@ -11,9 +13,15 @@ import java.util.*
 class DataContainer {
 
     companion object {
+
+        private lateinit var databaseReference : DatabaseReference
+        private lateinit var listOfNotes : ArrayList<DataBaseRow>
+
         var authentication: MutableLiveData<Boolean> = MutableLiveData<Boolean>()
         var registration: MutableLiveData<Boolean> = MutableLiveData<Boolean>()
         var allNotes:MutableLiveData<List<Note>> = MutableLiveData<List<Note>>()
+
+
 
         var userName:String=String()
         lateinit var currentNote:Note
@@ -25,6 +33,8 @@ class DataContainer {
 
         fun Refresh(){
             DatabaseHttpRequests.sendGetNotesRequest(userName)
+            val fireBase = FirebaseDatabase.getInstance()
+            databaseReference = fireBase.getReference("NoteIt")
         }
         fun GetBiggestId():Int{
             var value=0
